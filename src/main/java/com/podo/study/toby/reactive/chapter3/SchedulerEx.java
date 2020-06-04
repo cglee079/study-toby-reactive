8d4e521c2176d78c46b6d9de21af6ddbb3678a91
cglee079 <cglee079@zuminternet.com>
2020-06-04 11:45:58 +0900

    `subscribeOn()` 구현

--- src/main/java/com/podo/study/toby/reactive/chapter3/SchedulerEx.java
+++ src/main/java/com/podo/study/toby/reactive/chapter3/SchedulerEx.java
@@ -5,11 +5,34 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 내부구조를 지금 까지 파악,
  * 내부구조를 파악하는것은 중요!
  * <p>
  * # Scheduler는 리액티브프로그래밍의 또 다른 하나의 축
+ *
+ *
+ * Quest.
+ * 하나의 메인 쓰레드에서,
+ * publisher, subscriber, subscription 이 동작함.
+ *
+ * 실전에서는 쓸일이 필요 없음.
+ * 옵저버를 쓸려면, 보통 외부에서 이벤트가 발생함.
+ * 실전에서는, 이런 블록된 구조에서는 사용하지 않음.
+ *
+ * Answer.
+ * 리액티브에서 지원하는 Scheduler 를 이용해 해결하자.
+ *
+ * 1. subscribeOn(스케줄러)
+ * subscribe(), request(), onNext()..
+ * 데이터를 쏴주는 작업을, 지정된 스케줄러 안에서 수행하라.
+ * 메인 쓰레드를 블록킹하지않고, 다른 쓰레드에서, 데이터 생성.
+ * -> publisher 가 굉장히 느린 경우, 사용해라.
+ *
  */
 
 @Slf4j
@@ -20,12 +43,14 @@ public class SchedulerEx {
             sub.onSubscribe(new Subscription() {
                 @Override
                 public void request(long n) {
+                    log.debug("request : {}", n);
+
                     sub.onNext(1);
                     sub.onNext(2);
                     sub.onNext(3);
                     sub.onNext(4);
                     sub.onNext(5);
-                    sub.onComplete();;
+                    sub.onComplete();
                 }
 
                 @Override
@@ -35,7 +60,12 @@ public class SchedulerEx {
             });
         };
 
-        pub.subscribe(new Subscriber<Integer>() {
+        final Publisher<Integer> subscribeOnPub = sub -> {
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            es.execute(() -> pub.subscribe(sub));
+        };
+
+        subscribeOnPub.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription subscription) {
                 log.debug("On Subscribe");
@@ -57,5 +87,10 @@ public class SchedulerEx {
                 log.debug("On Complete");
             }
         });
+
+        log.debug("EXIT");
     }
+
+
 }
+
