fdc1f6a41f8951fd1037cad2e9a5c83841386c88
cglee079 <cglee079@zuminternet.com>
2020-06-04 14:20:14 +0900

    `publisheOn` 구현

--- src/main/java/com/podo/study/toby/reactive/chapter3/SchedulerEx.java
+++ src/main/java/com/podo/study/toby/reactive/chapter3/SchedulerEx.java
@@ -14,9 +14,9 @@ import java.util.concurrent.Executors;
  * 내부구조를 파악하는것은 중요!
  * <p>
  * # Scheduler는 리액티브프로그래밍의 또 다른 하나의 축
- *
- *
- * Quest.
+ * <p>
+ * <p>
+ * ## Quest.
  * 하나의 메인 쓰레드에서,
  * publisher, subscriber, subscription 이 동작함.
  *
@@ -24,7 +24,7 @@ import java.util.concurrent.Executors;
  * 옵저버를 쓸려면, 보통 외부에서 이벤트가 발생함.
  * 실전에서는, 이런 블록된 구조에서는 사용하지 않음.
  *
- * Answer.
+ * ## Answer.
  * 리액티브에서 지원하는 Scheduler 를 이용해 해결하자.
  *
  * 1. subscribeOn(스케줄러)
@@ -32,6 +32,11 @@ import java.util.concurrent.Executors;
  * 데이터를 쏴주는 작업을, 지정된 스케줄러 안에서 수행하라.
  * 메인 쓰레드를 블록킹하지않고, 다른 쓰레드에서, 데이터 생성.
  * -> publisher 가 굉장히 느린 경우, 사용해라.
+ *
+ * 2. publishOn(스케줄러)
+ * subscribe(), request()은 실행한 쪽에서 진행하고
+ * onNext()와 같이 데이터를 받는 subscribes()를 별도의 쓰레드에서 처리
+ * -> subscribe 가 굉장히 느린 경우, 사용해라.
  *
  */
 
@@ -50,6 +55,7 @@ public class SchedulerEx {
                     sub.onNext(3);
                     sub.onNext(4);
                     sub.onNext(5);
+
                     sub.onComplete();
                 }
 
@@ -60,16 +66,45 @@ public class SchedulerEx {
             });
         };
 
-        final Publisher<Integer> subscribeOnPub = sub -> {
-            ExecutorService es = Executors.newSingleThreadExecutor();
-            es.execute(() -> pub.subscribe(sub));
+//        final Publisher<Integer> subscribeOnPub = sub -> {
+//            ExecutorService es = Executors.newSingleThreadExecutor();
+//            es.execute(() -> pub.subscribe(sub));
+//        };
+
+        final Publisher<Integer> publishOnPub = sub -> {
+            pub.subscribe(new Subscriber<Integer>() {
+
+                final ExecutorService es = Executors.newSingleThreadExecutor();
+
+                @Override
+                public void onSubscribe(Subscription subscription) {
+                    sub.onSubscribe(subscription);
+                }
+
+                @Override
+                public void onNext(Integer i) {
+                    es.execute(() -> sub.onNext(i));
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    es.execute(() -> sub.onError(t));
+                }
+
+                @Override
+                public void onComplete() {
+                    es.execute(() -> sub.onComplete());
+                }
+            });
+
+
         };
 
-        subscribeOnPub.subscribe(new Subscriber<Integer>() {
+        publishOnPub.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription subscription) {
                 log.debug("On Subscribe");
-                subscription.request(Long.MAX_VALUE);
+                subscription.request(1);
             }
 
             @Override
