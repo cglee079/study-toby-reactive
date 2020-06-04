f46504135228b015fecd180888ed85cff697749c
cglee079 <cglee079@zuminternet.com>
2020-06-04 09:53:59 +0900

    pub/sub 구현, 멀티 쓰레드

--- src/main/java/com/podo/study/toby/reactive/chapter1/PubSub.java
+++ src/main/java/com/podo/study/toby/reactive/chapter1/PubSub.java
@@ -6,6 +6,8 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -19,7 +21,11 @@ import java.util.stream.Stream;
  * <p>
  * 1. onSubscribe : 최초 1회 반드시 실행
  * 2. onNext : 0 ~ N 실행
- * 3. onError || onComplete : 종료시 실행'
+ * 3. onError || onComplete : 종료 시 실행
+ * <p>
+ * 멀티스레드로 하면 동시성 문제가 발생하지않을까?
+ * 예를 들어, sub가 순서가 꼬여서 데이터를 받는다던가
+ * 따라서 onNext를 한 쓰레드에서만 호출하는것을 규약했다.
  */
 @Slf4j
 public class PubSub {
@@ -27,6 +33,7 @@ public class PubSub {
     public static void main(String[] args) {
         final Publisher<Integer> pub = new Publisher<Integer>() {
 
+            final ExecutorService es = Executors.newSingleThreadExecutor();
             final Iterable<Integer> iter = Arrays.asList(1, 2, 3, 4, 5);
 
             @Override
@@ -37,15 +44,17 @@ public class PubSub {
                         new Subscription() {
                             @Override
                             public void request(long n) { // n 은 몇개의 데이터를 요청 할지
-                                try {
-                                    for (Integer i : iter) {
-                                        sub.onNext(i);
-                                    }
-                                    sub.onComplete();
+                                es.execute(() -> {
+                                    try {
+                                        for (Integer i : iter) {
+                                            sub.onNext(i);
+                                        }
+                                        sub.onComplete();
 
-                                } catch (Exception e) {
-                                    sub.onError(e);
-                                }
+                                    } catch (Exception e) {
+                                        sub.onError(e);
+                                    }
+                                });
                             }
 
                             @Override
